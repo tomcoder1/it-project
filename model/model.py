e996f6f4327be37ff69ca3c871fae80b4faff985
Tom <103400335+tomcoder1@users.noreply.github.com>
2025-06-20 00:30:02 +0700

    Add files via upload

--- model/model.py
+++ model/model.py
@@ -1,32 +1,32 @@
-import onnxruntime as ort
+import tensorflow as tf
 import numpy as np
 from PIL import Image
 from .label import ilabel
-import os
 
-model_path = os.path.join(os.path.dirname(__file__), "food101.onnx")
-session = ort.InferenceSession(model_path)
+interpreter = tf.lite.Interpreter(model_path="model/food101_model.tflite")
+interpreter.allocate_tensors()
 
-def preprocess_image(path):
-    img = Image.open(path).convert("RGB").resize((224, 224))
-    img = np.array(img).astype(np.float32) / 255.0 
-    img = img.transpose(2, 0, 1)
-    img = np.expand_dims(img, 0)
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+def preprocess_image(image_path):
+    img = Image.open(image_path).convert("RGB").resize((128, 128))
+    img = np.asarray(img, dtype=np.float32) / 255.0
+    img = np.expand_dims(img, axis=0)
     return img
 
-def classify_food(path):
-    try:
-        input_tensor = preprocess_image(path)
-        outputs = session.run(None, {"pixel_values": input_tensor})
-        logits = outputs[0]
-        probs = softmax(logits)
-        top_index = np.argmax(probs)
+def classify_food(image_path):
+    input_data = preprocess_image(image_path)
 
-        return ilabel[top_index]
-    except Exception as e:
-        print(f"Error during ONNX inference: {e}")
-        return None
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])[0]
+    # probs = tf.nn.softmax(output_data).numpy()
+    # top_indices = np.argsort(output_data)[::-1][:5]
 
-def softmax(x):
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum(axis=1, keepdims=True)
+    # return [(ilabel[idx], probs[idx]) for idx in top_indices]
+    top_idx = np.argmax(output_data)
+    confidence = output_data[top_idx]
+
+    # label = ilabel[top_idx]
+    return ilabel[top_idx]
